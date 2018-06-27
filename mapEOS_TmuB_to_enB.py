722b6b78229fc2afcb9214dd93c24e54685f8e98
Chun Shen <chunshen1987@gmail.com>
2018-06-27 11:04:54 -0400

    add physical units ed and nB and Pressure in the script

--- mapEOS_TmuB_to_enB.py
+++ mapEOS_TmuB_to_enB.py
@@ -3,7 +3,8 @@
 from numpy import *
 import sys
 from os import path
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
 
 def inverse_mapping_2D(x, y, xt, yt, z1, z2, z3):
     """
@@ -17,7 +18,7 @@ def inverse_mapping_2D(x, y, xt, yt, z1, z2, z3):
     z3_cut = zeros(ny)
     for iy in range(ny):
         idx_x = searchsorted(xt[:, iy], x, side='left')
-        idx_x_array[iy] = max(0, idx_x - 1)
+        idx_x = max(0, min(nx - 2, idx_x - 1))
         x_1 = xt[idx_x, iy]
         x_2 = xt[idx_x + 1, iy]
         y_1 = yt[idx_x, iy]
@@ -32,10 +33,13 @@ def inverse_mapping_2D(x, y, xt, yt, z1, z2, z3):
         z1_cut[iy] = z1_1 + (z1_2 - z1_1)/(x_2 - x_1)*(x - x_1)
         z2_cut[iy] = z2_1 + (z2_2 - z2_1)/(x_2 - x_1)*(x - x_1)
         z3_cut[iy] = z3_1 + (z3_2 - z3_1)/(x_2 - x_1)*(x - x_1)
-    idx_y = max(0, searchsorted(y_cut, y, side='left') - 1)
+    idx_y = min(ny - 2, max(0, searchsorted(y_cut, y, side='left') - 1))
     z1_interp = z1_cut[idx_y] + (z1_cut[idx_y + 1] - z1_cut[idx_y])/(y_cut[idx_y + 1] - y_cut[idx_y])*(y - y_cut[idx_y])
     z2_interp = z2_cut[idx_y] + (z2_cut[idx_y + 1] - z2_cut[idx_y])/(y_cut[idx_y + 1] - y_cut[idx_y])*(y - y_cut[idx_y])
     z3_interp = z3_cut[idx_y] + (z3_cut[idx_y + 1] - z3_cut[idx_y])/(y_cut[idx_y + 1] - y_cut[idx_y])*(y - y_cut[idx_y])
+    z1_interp = max(0., z1_interp)
+    z2_interp = max(0., z2_interp)
+    z3_interp = max(0., z3_interp)
     return(z1_interp, z2_interp, z3_interp)
 
 
@@ -45,6 +49,8 @@ except:
     print("Usage: python3 mapEOS_TmuB_to_enB.py EOS_table_folder")
     exit(1)
 
+
+hbarC = 0.19733
 
 filename_string = EOS_table_folder.split("Files_")[1].split("/")[0]
 
@@ -61,14 +67,29 @@ muB = linspace(0.000, 0.450, n_muB)
 T   = linspace(0.030, 0.800, n_T)
 
 muB_table, T_table = meshgrid(muB, T)
-ed_table = ed_table[:, 2].reshape(n_T, n_muB)
-nB_table = nB_table[:, 2].reshape(n_T, n_muB)
-P_table  = P_table[:, 2].reshape(n_T, n_muB)
+ed_table = ed_table[:, 2].reshape(n_T, n_muB)*(T_table**4.)/(hbarC**3.)
+nB_table = nB_table[:, 2].reshape(n_T, n_muB)*(T_table**3.)/(hbarC**3.)
+P_table  = P_table[:, 2].reshape(n_T, n_muB)*(T_table**4.)/(hbarC**3.)
 
-ed_local = 1.0
-nB_local = 1e-3
+print(matrix(ed_table).min(), matrix(ed_table).max())
+print(matrix(nB_table).min(), matrix(nB_table).max())
 
-P_local, T_local, muB_local = inverse_mapping_2D(
-        ed_local, nB_local, ed_table, nB_table, P_table, T_table, muB_table)
+plt.figure()
+plt.contourf(muB_table.transpose(), T_table.transpose(), ed_table.transpose(), 50)
+plt.show()
 
-print(P_local, T_local, muB_local)
+plt.figure()
+plt.contourf(muB_table.transpose(), T_table.transpose(), nB_table.transpose(), 50)
+plt.show()
+
+ed_list = linspace(2e-5, 650, 500)
+nB_list = linspace(0.0, 11.5, 500)
+
+for ie in range(len(ed_list)):
+    ed_local = ed_list[ie]
+    for inB in range(len(nB_list)):
+        nB_local = nB_list[inB]
+        P_local, T_local, muB_local = inverse_mapping_2D(
+            ed_local, nB_local, ed_table, nB_table, P_table, T_table, muB_table)
+
+        print(ed_local, nB_local, P_local, T_local, muB_local)
